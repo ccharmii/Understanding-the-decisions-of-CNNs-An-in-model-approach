681ee77a960c7700d3c81ace36c2296d8dee8985
ccharmii <cutadash0219@naver.com>
2026-05-25 03:37:44 +0000

    chore: finetuning 스크립트 변경

--- xML/pytorch/run_finetuning.sh
+++ xML/pytorch/run_finetuning.sh
@@ -3,11 +3,10 @@
 
 
 python finetuning.py \
-  --dataset embryo \
+  --dataset hmc \
   --dataset_path "../data/" \
-  --checkpoint "./finetuning_pth/resnet34_best_loss.pt" \
-  --folder "./finetuning_output/new/embryo" \
-  --init_bias 3.0 \
+  --checkpoint "./finetuning_pth/2025-06-01_08-15-002_best_loss.pt" \
+  --folder "./finetuning_output/new/hmc" \
   --loss unsupervised \
   --alpha 1.0,0.25,0.9 \
   --beta 0.9 \
@@ -15,9 +14,10 @@ python finetuning.py \
   --lr_clf 0.01,0,0.01 \
   --lr_expl 0,0.0001,0.0001 \
   --aug_prob 0.2 \
-  --opt sgd -clf resnet34 \
+  --opt sgd \
+  -clf resnet18 \
   --batch_size 8 \
-  --early_patience 100,100,10 \
+  --early_patience 100,100,10
 
 ## 7/28 phase 1에서 explainer가 천천히 움직이도록 lr_expl 낮춤
 ## --> 원래: lr_expl 0,0.01,0.01
